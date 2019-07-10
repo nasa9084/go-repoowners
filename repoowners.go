463f425f1e991d9a11853bdc20687af2bf7bbfca
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-07-10 13:22:53 +0900

    split set

--- repoowners.go
+++ repoowners.go
@@ -4,8 +4,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"sort"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -253,71 +251,4 @@ func parseAliases(r io.Reader) (aliasesConfig, error) {
 		return aliasesConfig{}, err
 	}
 	return a, nil
-}
-
-// UsernameSet is a set type for usernames.
-type UsernameSet map[string]struct{}
-
-func newUsernameSet(usernames ...string) UsernameSet {
-	us := UsernameSet{}
-	us.Add(usernames...)
-	return us
-}
-
-// Add given usernames to the set.
-// This function is mutable.
-func (us UsernameSet) Add(usernames ...string) {
-	for _, username := range usernames {
-		us[username] = struct{}{}
-	}
-}
-
-// Delete given usernames from the set.
-// This function is mutable.
-func (us UsernameSet) Delete(usernames ...string) {
-	for _, username := range usernames {
-		delete(us, username)
-	}
-}
-
-// Union get a new set which contains the members of the set
-// and the members of given set.
-// This function is immutable.
-func (us UsernameSet) Union(us2 UsernameSet) UsernameSet {
-	result := UsernameSet{}
-	for k := range us {
-		result.Add(k)
-	}
-	for k := range us2 {
-		result.Add(k)
-	}
-	return result
-}
-
-// Has returns true if given username is a member of the set.
-func (us UsernameSet) Has(username string) bool {
-	_, has := us[username]
-	return has
-}
-
-// List returns a sorted list which contains members of set.
-func (us UsernameSet) List() []string {
-	ret := make([]string, 0, len(us))
-	for k := range us {
-		ret = append(ret, k)
-	}
-	return ret
-}
-
-func (us UsernameSet) String() string {
-	var buf strings.Builder
-	buf.WriteString("{")
-	usernames := us.List()
-	for i := range usernames {
-		usernames[i] = strconv.Quote(usernames[i])
-	}
-	sort.Strings(usernames)
-	buf.WriteString(strings.Join(usernames, ", "))
-	buf.WriteString("}")
-	return buf.String()
 }
